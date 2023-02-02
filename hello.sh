4374044d1f88fb94d33d26f97bbc89c9535ddb40
Nandini Chhajed <61181640+nandinichhajed@users.noreply.github.com>
2023-02-02 18:49:08 +0530

    Add files via upload

---
+++ hello.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/bash
+
+echo "Hello world!"
+
+
+# here we have used "which bash" command to get the bash path
+# give the executable permissions to the file
+# chmod +x hello.sh
+# executing the file ./filename.shs
