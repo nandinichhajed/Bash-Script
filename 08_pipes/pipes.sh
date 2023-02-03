9fbd1d634f09d1dce307ea40ad93a33431dee9d3
Nandini Chhajed <61181640+nandinichhajed@users.noreply.github.com>
2023-02-03 12:58:20 +0530

    Sending output from one script to another

---
+++ 08_pipes/pipes.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/bash
+
+#send output form one script to another
+
+message="Hii this is nandini's system"
+
+export message
+./secondScript.sh
---
+++ 08_pipes/secondScript.sh
@@ -0,0 +1,3 @@
+#! /usr/bin/bash
+
+echo "the message recieved is: $message"
