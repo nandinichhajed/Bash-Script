2b555ea0a5a09ffb2705947c0217987645cf2382
Nandini Chhajed <61181640+nandinichhajed@users.noreply.github.com>
2023-02-06 11:34:41 +0530

    basic numeric operations

---
+++ 10_Numbers_and_arthematic/basics.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/bash
+
+n1=20
+n2=5
+
+echo $(( n1 + n2 ))
+echo $(( n1 - n2 ))
+echo $(( n1 * n2 ))
+echo $(( n1 / n2 ))
+echo $(( n1 % n2 ))
+echo $(( n1 ** n2 ))
+
+# by using expr
+echo $(expr $n1 + $n2 )
+echo $(expr $n1 - $n2 )
+echo $(expr $n1 \* $n2 )
+echo $(expr $n1 / $n2 )
+echo $(expr $n1 % $n2 )
