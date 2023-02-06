bf0cebf4373be17837c7024208c891a925057ce1
Nandini Chhajed <61181640+nandinichhajed@users.noreply.github.com>
2023-02-06 12:15:23 +0530

    declearing and restricting variables

---
+++ 11_DeclareCommand/declaring.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/bash
+
+
+# declare - bash build in command that allows you to update attribute apply to variable within the scope of your shell
+
+# use to decleare variable in long run
+
+declare -p
+# show all the vaeiable present in system
+
+declare myvariable
+# declaring our own variable
+
+declare myvariable=22
+# assigining value to our variable
+
+declare myvariable=11
+# changing value of our variable
---
+++ 11_DeclareCommand/restricting_file.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/bash
+
+declare -r pwdfile=/etc/pswd
+
+echo $pwdfile
+
+pswd=/etc/acg.txt
+
+# it will print the same path /etc/pswd as -r is for read mode only
+echo $pwdfile
